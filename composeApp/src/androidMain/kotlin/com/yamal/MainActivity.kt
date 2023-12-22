76f8776fb67489dddd86e142baed00d43f91a842
Davide Magli <davide.magli99@gmail.com>
2023-12-22 23:05:29 +0100

    handling login intents for ios and android

--- composeApp/src/androidMain/kotlin/com/yamal/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/yamal/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.edivad1999.yamal
+package com.yamal
 
 import App
 import android.os.Bundle
@@ -6,10 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import screen.login.LoginUtilities
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (intent.data?.toString()?.startsWith("yamal") == true) {
+            intent.data?.let { LoginUtilities.parseUrlResult(it.toString()) }
+        }
+
         super.onCreate(savedInstanceState)
         setContent {
             App()
