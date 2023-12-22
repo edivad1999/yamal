6dc7335df511c619808ec2d7f5ea3c306f24eeee
Davide Magli <davide.magli99@gmail.com>
2023-12-22 23:06:07 +0100

    handling login intents for ios and android

--- composeApp/src/desktopMain/kotlin/screen/login/LoginUtilities.desktop.kt
+++ composeApp/src/desktopMain/kotlin/screen/login/LoginUtilities.desktop.kt
@@ -2,11 +2,8 @@ package screen.login
 
 import org.koin.core.component.KoinComponent
 
-actual object LoginUtilities : KoinComponent {
+actual object LoginUtilities : KoinComponent, AuthCodeHandler() {
 
     actual suspend fun launchBrowser(url: String) {
-    }
-
-    actual fun parseUrlResult(urlResult: String) {
     }
 }
