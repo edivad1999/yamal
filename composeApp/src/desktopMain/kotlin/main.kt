528838b0701c33822db5ad09d4050291073f3cc2
Davide Magli <davide.magli99@gmail.com>
2024-01-05 14:44:02 +0100

    fixed desktop

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,65 +1,20 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview // ktlint-disable filename
-import androidx.compose.material.Text
+@file:Suppress("ktlint:standard:filename")
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import dev.datlag.kcef.KCEF
-import java.io.File
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import di.koinInitializer
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        var restartRequired by remember { mutableStateOf(false) }
-        var downloading by remember { mutableStateOf(0F) }
-        var initialized by remember { mutableStateOf(false) }
-
-        LaunchedEffect(Unit) {
-            withContext(Dispatchers.IO) {
-                KCEF.init(builder = {
-                    installDir(File("kcef-bundle"))
-                    progress {
-                        onDownloading {
-                            downloading = it
-                        }
-                        onInitialized {
-                            initialized = true
-                        }
-                    }
-                    settings {
-                        cachePath = File("cache").absolutePath
-                    }
-                }, onError = {
-                    it?.printStackTrace()
-                }, onRestartRequired = {
-                    restartRequired = true
-                })
-            }
-        }
-
-        if (restartRequired) {
-            Text(text = "Restart required.")
-        } else {
-            if (initialized) {
-                App()
-            } else {
-                Text(text = "Downloading $downloading%")
-            }
-        }
-
-        DisposableEffect(Unit) {
-            onDispose {
-                KCEF.disposeBlocking()
-            }
+fun main() =
+    application {
+        koinInitializer { }
+        Window(onCloseRequest = ::exitApplication) {
+            App()
         }
     }
-}
 
 @Preview
 @Composable
